93aaef31cb1086e91fc0828a1ad03f9c6c0ef564
Yasushi Ichikawa <ichikaway@gmail.com>
2015-05-19 21:34:37 +0900

    remove

--- vaddy_libs.rb
+++
@@ -1,6 +0,0 @@
-def build_query(query_hash = {})
-    query_string = (query_hash||{}).map{|k,v|
-          URI.encode(k.to_s) + "=" + URI.encode(v.to_s)
-          }.join("&")
-    return query_string 
-end
